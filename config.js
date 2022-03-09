ac23d59407a041891bcf477bf528e0ac459c7d00
alex952 <alex952.anf5@gmail.com>
2022-03-09 11:24:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Alex',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
@@ -32,8 +32,8 @@ const CONFIG = {
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '38.3626798',
+  defaultLongitude: '-0.4793588',
 
   // Autochange
   autoChangeTheme: true,
