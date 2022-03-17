480f57af01887f33d97616d82a1baa1c49aa2948
alex952 <alex952.anf5@gmail.com>
2022-03-17 09:16:14 +0100

    Add openweathermap api key

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ const CONFIG = {
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherKey: '6dd2ffc9927f9c20c5974670ccbeb78c', // Write here your API Key
+  weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
